1e9bc8b0c8f119a8ed6c7449532105497dc7fdbd
Joanilza Bockhorny <jbockhorny@gmail.com>
2019-11-18 11:05:22 -0300

    form do registro

--- src/main.js
+++ src/main.js
@@ -1,70 +1,43 @@
 import Login from './pages/login.js';
 import Register from './pages/register.js';
-import Home from './pages/home.js';
+// import Home from './pages/home.js';
 import Event from './pages/event.js';
-import Profile from './pages/profile.js';
+// import Profile from './pages/profile.js';
 
 const locationHashChanged = () => {
   switch (location.hash) {
     case '#login':
-      if (firebase.auth().currentUser) {
-        window.location = '#home';
-      } else {
+          
         document.querySelector('main').innerHTML = Login();
-      }
+      
       break;
     case '#register':
-      if (firebase.auth().currentUser) {
-        window.location = '#home';
-      } else {
+     
+     
         document.querySelector('main').innerHTML = Register();
-      }
+      
       break;
     case '#home':
-      if (!firebase.auth().currentUser) {
-        window.location = '#login';
-      } else {
-        firebase.firestore().collection('events').orderBy('addedAt', 'desc')
-          .onSnapshot((snap) => {
-            document.querySelector('main').innerHTML = Home({ events: snap });
-          })
-      }
+    
+      document.querySelector('main').innerHTML = Event();
+       
       break;
-    case '#profile':
-      if (!firebase.auth().currentUser) {
-        window.location = '#login';
-      } else {
-        firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid)
-          .get().then((snap) => {
-            document.querySelector('main').innerHTML = Profile({ users: snap.data() });
-          })
-      }
-      break;
-    case '#event':
-      if (!firebase.auth().currentUser) {
-        window.location = '#login';
-      } else {
-        firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid)
-          .get()
-          .then((snap) => {
-            document.querySelector('main').innerHTML = Event({ users: snap.data() });
-          });
-      }
-      break;
-    default:
-      window.location = '#login';
+  
+    // default:
+    //   window.location = '#login';
   }
 };
 
-const init = (user) => {
-  if (!user) {
-    window.location = '#login';
-  } else {
-    window.location = '#home';
-    locationHashChanged();
-  }
-};
+// const init = (user) => {
+//   if (!user) {
+//     window.location = '#login';
+//   } else {
+//     window.location = '#home';
+//     locationHashChanged();
+//   }
+// };
 
-firebase.auth().onAuthStateChanged(init);
+// firebase.auth().onAuthStateChanged(init);
 
 window.addEventListener('hashchange', locationHashChanged, false);
+window.addEventListener('load', locationHashChanged, false);
