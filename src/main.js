50c82d31d4d7f9d3a919221ad179bca44016a4cb
juhsaolno <juanita.hsolano@gmail.com>
2019-11-18 16:31:43 -0300

    correcao main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Login from './pages/login.js';
 import Register from './pages/register.js';
-// import Home from './pages/home.js';
+import Home from './pages/home.js';
 import Event from './pages/event.js';
-// import Profile from './pages/profile.js';
+import Profile from './pages/profile.js';
+
 
 const locationHashChanged = () => {
   switch (location.hash) {
@@ -12,33 +13,38 @@ const locationHashChanged = () => {
     case '#register':
       document.querySelector('main').innerHTML = Register();
       break;
-    case '#event':
-      // firebase
-      //   .firestore()
-      //   .collection('users').doc(firebase.auth().currentUser.uid)
-      //   .get().then((snap) => {
-      //     document.querySelector('main').innerHTML = Event({
-      //       users: snap.data()
-      //     })
-      //   })
-      document.querySelector('main').innerHTML = Event();
+    case '#home':
+      document.querySelector('main').innerHTML = Home();
       break;
-
-    // default:
-    //   window.location = '#login';
+    case '#profile':
+      document.querySelector('main').innerHTML = Profile();
+      break;
+    case '#event':
+      firebase
+        .firestore()
+        .collection('users').doc(firebase.auth().currentUser.uid)
+        .get().then((snap) => {
+          document.querySelector('main').innerHTML = Event({
+            users: snap.data()
+          })
+        })
+      break;
+    default:
+      window.location = '#login';
   }
 };
 
-// const init = (user) => {
-//   if (!user) {
-//     window.location = '#login';
-//   } else {
-//     window.location = '#home';
-//     locationHashChanged();
-//   }
-// };
+const init = (user) => {
+  if (!user) {
+    window.location = '#login';
+    locationHashChanged();
+  } else {
+    window.location = '#home';
+    locationHashChanged();
+  }
+};
 
-// firebase.auth().onAuthStateChanged(init);
+firebase.auth().onAuthStateChanged(init);
 
 window.addEventListener('hashchange', locationHashChanged, false);
 window.addEventListener('load', locationHashChanged, false);
